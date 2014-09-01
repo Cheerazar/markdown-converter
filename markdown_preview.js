24847c371bc10330c0d2f95feaf8a8eac15a51ab
Antonio Dangond is awesome!!! :) yt & Gunnari Auvinen <pair+antoniodangond.cheerazar@devbootcamp.com>
2014-09-01 16:55:26 -0700

    Create addListener and convertMarkdown functions for Controller prototype.

--- markdown_preview.js
+++ markdown_preview.js
@@ -1,5 +1,4 @@
 var View = function() {
-
 };
 
 View.prototype = {
@@ -8,4 +7,25 @@ View.prototype = {
   }
 };
 
+
+var Controller = function(view) {
+  this.view = view
+}
+
+Controller.prototype = {
+  addListener: function() {
+    $('[name="markdown-input"]').on('keyup', this.convertMarkdown.bind(this));
+  },
+
+  convertMarkdown: function (event) {
+    var textToConvert = $(event.target).val();
+    this.view.renderView(textToConvert);
+  }
+}
+
+
+
 var view = new View();
+
+var controller = new Controller(view);
+controller.addListener();
